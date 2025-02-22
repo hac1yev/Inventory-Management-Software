83a610c619b29d0a88729047ba92eb58551d2cfe
hac1yev <ilkinhaciyev955@gmail.com>
2025-02-22 22:16:41 +0400

    text input component

--- app/(back-office)/dashboard/inventory/categories/new/page.tsx
+++ app/(back-office)/dashboard/inventory/categories/new/page.tsx
@@ -1,26 +1,47 @@
+"use client";
+
 import InventoryFormHeader from "@/components/dashboard/InventoryFormHeader";
-  
+import TextInput from "@/components/FormInputs/TextInput";
+import { useForm } from "react-hook-form";
+
 const NewCategory = () => {
-    return (
-        <div>
-            <InventoryFormHeader title="New Category" href="/" />
-            <form action="" className="w-full max-w-6xl my-4 mx-auto p-4 bg-white border border-gray-200 rounded-lg shadow-sm sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
-                <div className="grid gap-4 sm:grid-cols-2 sm:gap-6">
-                    <div className="sm:col-span-2">
-                        <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Product Name</label>
-                        <input type="text" name="name" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" />
-                    </div>
-                    <div className="w-full">
-                        <label htmlFor=""></label>
-                        <input type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" />
-                    </div>
-                </div>
-                <div className="mt-6 sm:col-span-1">
-                    <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
-                </div>
-            </form>
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <div>
+      <InventoryFormHeader title="New Category" href="/" />
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="w-full max-w-6xl my-4 mx-auto p-4 bg-white border border-gray-200 rounded-lg shadow-sm sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700"
+      >
+        <div className="grid gap-4 sm:grid-cols-2 sm:gap-6">
+          <TextInput 
+            label="Category Title"
+            name="title"
+            register={register}
+            errors={errors}
+          />
         </div>
-    );
+        <div className="mt-6 sm:col-span-1">
+          <button
+            type="submit"
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Save
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default NewCategory;
